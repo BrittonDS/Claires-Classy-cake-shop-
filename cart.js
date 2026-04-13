e2fdbc7bd689504a52f833ca2944ad93dd821e66
hbbrandclothing <162912247+hbbrandclothing@users.noreply.github.com>
2026-04-13 11:54:23 +0100

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,10 +1,38 @@
+// Get basket
 function getCart() {
   return JSON.parse(localStorage.getItem("cart")) || [];
 }
 
+// Save basket
+function saveCart(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Add item
 function addToCart(product) {
   let cart = getCart();
-  cart.push(product);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to basket!");
+
+  // Check if item already exists
+  let existing = cart.find(item => item.name === product.name);
+
+  if (existing) {
+    existing.quantity += 1;
+  } else {
+    cart.push({
+      name: product.name,
+      price: product.price,
+      quantity: 1
+    });
+  }
+
+  saveCart(cart);
+  alert(product.name + " added to basket!");
+}
+
+// Remove item
+function removeFromCart(name) {
+  let cart = getCart();
+  cart = cart.filter(item => item.name !== name);
+  saveCart(cart);
+  location.reload(); // refresh page
 }
